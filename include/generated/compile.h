393fdb210b4941882ee3d4490676959782dec2b7
sbryan12144 <sbryan12144@gmail.com>
2012-06-09 09:39:15 -0400

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Wed Jun 6 17:51:17 EDT 2012"
+#define LINUX_COMPILE_TIME "17:51:17"
+#define LINUX_COMPILE_BY "bryan"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "ubuntu-domain"
+#define LINUX_COMPILER "gcc version 4.5.4 20111003 (prerelease) (Linaro GCC 4.5-2011.10) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "2.6.35.14-BLACKOUT-ACE-GB-v1.0"
